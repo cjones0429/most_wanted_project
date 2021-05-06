231b5403d6dc333284f7e35c6a5934db9925bc28
unknown <mrchrisfarrell@yahoo.com>
2021-05-06 18:17:27 -0400

    last commit errored

--- app.js
+++ app.js
@@ -20,3 +20,42 @@ function searchByName(){
         console.log('Sorry, looks like there is no one with that name.');
     }
 }
+
+function searchBySingle(){
+    var userSelection = document.forms['singleForm']['selection'].value;
+    let userInput = document.forms['singleForm']['prop-value'].value;
+
+    let filteredPeople = people.filter(function (person) {
+        if(person.userSelection === userInput){
+            return true;
+        }
+        return false;
+    });
+    
+    if(filteredPeople.length > 0){
+        console.log(filteredPeople);
+    }else{
+        console.log('Sorry, looks like there is no one with that name.');
+    }
+}
+
+
+//turn data sheet into table//
+
+function displayTable(object){
+    object.map(function(el){
+        document.getElementById("data").innerHTML += `<tr>
+        <td>${el.id}</td>
+        <td>${el.firstName}</td>
+        <td>${el.lastName}</td>
+        <td>${el.gender}</td>
+        <td>${el.dob}</td>
+        <td>${el.height}</td>
+        <td>${el.weight}</td>
+        <td>${el.eyeColor}</td>
+        <td>${el.occupation}</td>
+        <td>${el.parents}</td>
+        <td>${el.currentSpouse}</td>`
+})
+}
+displayTable(people);
