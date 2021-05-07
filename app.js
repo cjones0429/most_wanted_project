96e681c6101d5075104822ceadf6061cb61b886a
unknown <mrchrisfarrell@yahoo.com>
2021-05-07 11:42:06 -0400

    added function to display updated table. Both index and single selection pages now load with starting table and update when form is submitted.

--- app.js
+++ app.js
@@ -16,32 +16,15 @@ function searchByName(){
     // Rather than console logging, you need to append the filteredPeople to a table.
     if(filteredPeople.length > 0){
         console.log(filteredPeople);
+        displayUpdatedTable(filteredPeople);
     }else{
         console.log('Sorry, looks like there is no one with that name.');
         alert('Sorry, looks like there is no one with that name.');
     }
-    displayTable(filteredPeople);
+
 }
 
-function searchBySingle(){           //function is called when form is submitted. Searches for matches, makes new array, posts results in table.
-    var userSelection = document.forms['singleForm']['selection'].value;
-    let userInput = document.forms['singleForm']['prop-value'].value;
 
-    let filteredPeople = people.filter(function (person) {
-        if(person[userSelection] === userInput){
-            return true;
-        }
-        return false;
-    });
-    
-    if(filteredPeople.length > 0){
-        console.log(filteredPeople);
-    }else{
-        console.log('Sorry, no match.');
-        alert('Sorry, no match.');
-    }
-    displayTable(filteredPeople);
-}
 
 
 //turn data sheet into table//
@@ -62,3 +45,43 @@ function displayTable(object){
         <td>${el.currentSpouse}</td>`
 })
 }
+
+function displayUpdatedTable(object){
+    document.getElementById("data").innerHTML = ``;
+    object.map(function(el){
+        document.getElementById("data").innerHTML += `<tr>
+        <td>${el.id}</td>
+        <td>${el.firstName}</td>
+        <td>${el.lastName}</td>
+        <td>${el.gender}</td>
+        <td>${el.dob}</td>
+        <td>${el.height}</td>
+        <td>${el.weight}</td>
+        <td>${el.eyeColor}</td>
+        <td>${el.occupation}</td>
+        <td>${el.parents}</td>
+        <td>${el.currentSpouse}</td>`
+})
+}
+
+function searchBySingle(){           //function is called when form is submitted. Searches for matches, makes new array, posts results in table.
+    var userSelection = document.forms['singleForm']['selection'].value;
+    let userInput = document.forms['singleForm']['prop-value'].value;
+
+    let filteredPeople = people.filter(function (person) {
+        if(person[userSelection] === userInput){
+            return true;
+        }
+        return false;
+    });
+    
+    if(filteredPeople.length > 0){
+        console.log(filteredPeople);
+        displayUpdatedTable(filteredPeople);
+    }else{
+        console.log('Sorry, no match.');
+        alert('Sorry, no match.');
+    }
+}
+
+displayTable(people);
